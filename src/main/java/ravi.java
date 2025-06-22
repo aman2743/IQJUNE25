9fef8386282d8b490d25392d2731874681dc57ee
buntykumar1810 <buntykumar1810@gmail.com>
2025-06-22 14:23:50 +0530

    2st commit

--- src/main/java/ravi.java
+++ src/main/java/ravi.java
@@ -1,7 +1,6 @@
 public class ravi {
     public static void main(String[] args) {
         System.out.println("word hello");
-        System.out.println("Helooo jbjeb");
-        System.out.println("rhfgbk");
+
     }
 }
