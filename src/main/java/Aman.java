e6812484e77e6afed4d26624858c1aada0a90c67
AmanSrivastava01 <amansri004@gmail.com>
2025-06-22 13:56:34 +0530

    Merge pull request #1 from
    aman this commit

--- src/main/java/Aman.java
+++ src/main/java/Aman.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Aman {
     public static void main(String[] args) {
-        System.out.println("Hellow i am Aman");
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        // Input: number of units
+        System.out.print("Enter the number of units used: ");
+        int units = scanner.nextInt();
+
+        // Nested if-else without logical operators
+        if (units < 100) {
+            System.out.println("Low Usage");
+        } else {
+            if (units <= 300) {
+                if (units % 10 == 0) {
+                    System.out.println("Moderate - Efficient Usage");
+                } else {
+                    System.out.println("Moderate Usage    ");
+                }
+            } else {
+                System.out.println("High Usage");
+            }
+        }
+
+        scanner.close();
+    }
 }
