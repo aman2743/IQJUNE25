367214e2ad4120d5371ee296bd72cead43e52747
lakshmigautam <gautamlakshmii17@gmail.com>
2025-06-22 14:10:53 +0530

    commit from Terminal

--- src/main/java/DemoTest.java
+++ src/main/java/DemoTest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class DemoTest {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        System.out.println("Enter your name:");
+        System.out.println("Enter your name :");
         String name = a.nextLine();
         System.out.println(name);
     }
