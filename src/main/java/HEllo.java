26fb150ef1db84b5d5a7750af7e0d44627561c65
khushietawh <khushisinghetw405@gmail.com>
2025-06-22 14:14:25 +0530

    yes sir

--- src/main/java/HEllo.java
+++ src/main/java/HEllo.java
@@ -2,6 +2,5 @@ public class HEllo {
     public static void main(String[] args) {
         System.out.println("Hello Sir");
         System.out.println("Hello rahul");
-        System.out.println("hello word");
     }
 }
