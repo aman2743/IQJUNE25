1c8a12cab49c7b254445ef6560ca5d94f2c644ac
khushietawh <khushisinghetw405@gmail.com>
2025-06-22 14:23:59 +0530

    second-commit

--- src/main/java/HEllo.java
+++ src/main/java/HEllo.java
@@ -2,5 +2,7 @@ public class HEllo {
     public static void main(String[] args) {
         System.out.println("Hello Sir");
         System.out.println("Hello rahul");
+        System.out.println("hello word");
+
     }
 }
