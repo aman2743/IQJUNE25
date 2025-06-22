58424d836469bfe943e36ed95354b4a116522258
aman2743 <161856146+aman2743@users.noreply.github.com>
2025-06-22 14:18:12 +0530

    1st commit

--- src/main/java/DemoTest.java
+++ src/main/java/DemoTest.java
@@ -1,5 +1,9 @@
 public class DemoTest {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a=10;
+        int  b=20;
+
+        System.out.println(a+b);
+        System.out.println("hello");
     }
 }
