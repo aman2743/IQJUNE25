69b6b39a054dd614c424e2ef5779ebcff33225ba
aadesh-pro <aadeshsagar555@gmail.com>
2025-06-22 13:55:26 +0530

    CreatedByAadeshh

--- src/main/java/CreatedByAadesh.java
+++ src/main/java/CreatedByAadesh.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CreatedByAadesh {
+public class CreatedByAadeshS {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
