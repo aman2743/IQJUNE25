3c4c6a6a933d26dfc95c69c5e92423e6ec14099d
ajay yadav <ajayyadav18102000@gmail.com>
2025-06-28 16:02:28 +0530

    Merge pull request #1 from aman2743/AMANK
    aman commit
    this is my first git program

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -2,5 +2,6 @@ public class Cal {
     public static void main(String[] args) {
         System.out.println("Java");
         System.out.println("this is my firt java program");
+        System.out.println("or  bhai log kese  ho");
     }
 }
