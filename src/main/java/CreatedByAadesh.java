768191f39d6fa0fbd5b6a4d5acecaa57e5165b7a
aadesh-pro <aadeshsagar555@gmail.com>
2025-06-28 16:04:49 +0530

    CreatedByAadeshh

--- src/main/java/CreatedByAadesh.java
+++ src/main/java/CreatedByAadesh.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
-public class CreatedByAadeshS {
+public class CreatedByAadesh {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter first number: ");
+//        int num1 = sc.nextInt();
+//
+//        System.out.print("Enter second number: ");
+//        int num2 = sc.nextInt();
+//
+//        int sum = num1 + num2;
+//
+//        System.out.println("The sum is: " + sum);
 
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
+        // Swap number
 
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
 
-        int sum = num1 + num2;
 
-        System.out.println("The sum is: " + sum);
+
     }
 }
+
