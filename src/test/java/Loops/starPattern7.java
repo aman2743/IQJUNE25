cd07e974d29753b0819537622ecb612c7a4e49d8
Aman Kumar <eraman.kush@gmail.com>
2025-07-05 17:52:52 +0530

    Loop Commit on 5th July

--- src/test/java/Loops/starPattern7.java
+++ src/test/java/Loops/starPattern7.java
@@ -1,15 +1,15 @@
 package Loops;
 
-public class starPattern3 {
+public class starPattern7 {
     public static void main(String[] args) {
-        for (int i = 0; i <5 ; i++) {
-            for (int j = i; j <5 ; j++) {
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <=i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = 1; i <5 ; i++) {
-            for (int j = 0; j <=i ; j++) {
+        for (int i = 3; i >= 0; i--) {
+            for (int j = i; j >=0; j--) {
                 System.out.print("*");
             }
             System.out.println();
