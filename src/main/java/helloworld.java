096459fd82b970eda3ba2fea5af60fb729bf9c18
Neha Shah <nehabansal5sep@gmail.com>
2025-07-06 13:13:23 +0530

    hello world

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,6 +1,5 @@
-public class helloworld  {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
-
+        System.out.println(" World Neha");
     }
 }
