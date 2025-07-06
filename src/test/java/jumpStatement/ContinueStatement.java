d6c412dce81ccd5bcd87de28e3a0fada2a760b66
Aman Kumar <eraman.kush@gmail.com>
2025-07-06 14:01:01 +0530

    Jump statement Commit on 6th July

---
+++ src/test/java/jumpStatement/ContinueStatement.java
@@ -0,0 +1,12 @@
+package jumpStatement;
+
+public class ContinueStatement {
+    public static void main(String[] args) {
+        for (int i = 0; i <20 ; i++) {
+            if(i==10){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
